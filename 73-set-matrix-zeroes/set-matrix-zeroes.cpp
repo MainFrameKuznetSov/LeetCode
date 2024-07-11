839a54e94bed0adf68a875992abfca20a9a17461
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-11 11:34:49 +0530

    Time: 12 ms (39.77%) | Memory: 16.4 MB (93.99%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& mat) {
         int n=mat.size(),m=mat[0].size();
-        vector<int>row,col;
+        vector<pair<int,int>>cell;
         for(int i=0;i<n;++i)
         {
             for(int j=0;j<m;++j)
             {
                 if(mat[i][j]==0)
-                {
-                    row.push_back(i);
-                    col.push_back(j);
-                }
+                    cell.push_back(make_pair(i,j));
             }
         }
-        for(int i=0;i<row.size();++i)
+        for(int i=0;i<cell.size();++i)
         {
             for(int j=0;j<m;++j)
-                mat[row[i]][j]=0;
-        }
-        for(int i=0;i<col.size();++i)
-        {
+                mat[cell[i].first][j]=0;
             for(int j=0;j<n;++j)
-                mat[j][col[i]]=0;
+                mat[j][cell[i].second]=0;
         }
     }
 };
