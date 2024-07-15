eca67c79922aaff79548fe46f67d7eb08147a786
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-15 20:28:03 +0530

    Time: 5 ms (36.44%) | Memory: 10.5 MB (6.35%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>&grid) {
+        int m=grid.size(),n=grid[0].size();
+        /*if(m==1 && n==1)
+        {   
+            if(grid[0][0]==1)
+                return 0;
+            else
+                return 1;
+        }*/
+        vector<int>prev(n,0);
+        if(grid[0][0]==1)
+            prev[0]=0;
+        else
+            prev[0]=1;
+        for(int i=0;i<m;++i)
+        {
+            vector<int>curr(n,0);
+            curr=prev;
+            for(int j=0;j<n;++j)
+            {
+                int up=0,left=0;
+                if(i==0 && j==0)
+                    continue;
+                if(i>0 && grid[i][j]!=1)
+                    up=prev[j];
+                if(j>0 && grid[i][j]!=1)
+                    left=curr[j-1];
+                curr[j]=up+left;
+            }
+            prev=curr;
+        }
+        return prev[n-1];
+    }
+};
