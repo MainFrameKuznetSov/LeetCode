238b114f75b341cbc705d4b6751e96b96c1e1ee6
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-18 22:14:18 +0530

    Time: 238 ms (35.32%) | Memory: 294.9 MB (8.35%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int memo(int ind,int prev,int n,vector<int>&arr,vector<vector<int>>&dp)
-    {
-        if(ind==n)
-            return 0;
-        if(dp[ind][prev+1]!=-1)
-            return dp[ind][prev+1];
-        int len=memo(ind+1,prev,n,arr,dp);
-        if(prev==-1 || arr[ind]>arr[prev])
-            len=max(len,1+memo(ind+1,ind,n,arr,dp));
-        return dp[ind][prev+1]=len;
-    } 
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return memo(0,-1,n,nums,dp);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=n-1;i>=0;--i)
+        {
+            for(int j=i-1;j>=-1;--j)
+            {
+                int len=dp[i+1][j+1];
+                if(j==-1 || nums[i]>nums[j])
+                    len=max(len,1+dp[i+1][i+1]);
+                dp[i][j+1]=len;
+            }
+        }
+        return dp[0][0];
     }
 };
