cc84028ceff7e98380fd29656630d915ce726b57
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-28 20:34:37 +0530

    Time: 247 ms (45.60%) | Memory: 19.7 MB (6.19%) - LeetSync

---
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>> dp(n,vector<int>(n,-1));
+        vector<int>prev(n,-1);
+        for(int i=0;i<n;i++)
+            prev[i]=grid[0][i];
+        int ans=INT_MAX;
+        for(int i=1;i<n;i++)
+        {
+            vector<int>curr(n,-1);
+            for(int j=0;j<n;j++)
+            {
+                int mn=INT_MAX;
+                for(int k=0;k<n;k++)
+                {
+                    if(k!=j) 
+                        mn=min(mn,grid[i][j]+prev[k]);
+                    curr[j]=mn;
+                }
+            }
+            prev=curr;
+        }
+        for(int i=0;i<n;i++)
+            ans=min(ans,prev[i]);
+        return ans;
+    }
+};
