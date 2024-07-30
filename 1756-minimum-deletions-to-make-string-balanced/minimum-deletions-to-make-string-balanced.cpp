d6be19de215f6700c1a020d298cf1ef77c1962e4
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-07-30 18:49:51 +0530

    Time: 77 ms (87.56%) | Memory: 23.7 MB (66.99%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int ans=0,cnt=0;
+        for(char c:s)
+        {
+            if(c=='b')  
+                ++cnt;
+            else  
+                ans=min(ans+1,cnt);
+        }
+        return ans;
+    }
+};
