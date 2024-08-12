c92ab796c5bbf7eba63af9d7136419c7caa46146
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-08-12 19:21:45 +0530

    Time: 22 ms (86.49%) | Memory: 24.8 MB (39.10%) - LeetSync

---
+++ 789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,22 @@
+
+class KthLargest {
+public:
+    int k;
+    priority_queue<int,vector<int>,greater<int>>minHeap;
+    KthLargest(int k,vector<int>& nums) {
+        this->k=k;
+        for(int iter:nums)
+            add(iter);
+    }
+
+    int add(int val) {
+        if (minHeap.size()<k) 
+            minHeap.push(val);
+        else if(val>minHeap.top()) 
+        {
+            minHeap.pop();
+            minHeap.push(val);
+        }
+        return minHeap.top();
+    }
+};
