df3060c309e02d46f7b6cd044d95745d4d40377a
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-08-18 11:38:44 +0530

    Time: 101 ms (9.82%) | Memory: 35.5 MB (7.92%) - LeetSync

---
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int fact[]={2,3,5};
+        priority_queue<long long,vector<long long>,greater<long long>>heap;
+        set<long long>vis;
+        heap.push(1);
+        vis.insert(1);
+        long long ans;
+        for(int i=0;i<n;++i)
+        {
+            ans=heap.top();
+            heap.pop();
+            for(int i:fact)
+            {
+                long long temp=ans*i;
+                if(vis.find(temp)==vis.end())
+                {
+                    heap.push(temp);
+                    vis.insert(temp);
+                }
+            }
+        }
+        return ans;
+    }
+};
