cb86fe685b22a03c1667fcfdfe43adc4fff67795
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-08-26 12:31:59 +0530

    Time: 272 ms (5.03%) | Memory: 62.7 MB (53.49%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n=graph.size();
+        vector<int>adj[n],indeg(n,0),topo;
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<graph[i].size();++j)
+                adj[graph[i][j]].emplace_back(i);
+        }
+        for(int i=0;i<n;++i)
+        {
+            cout<<i<<"--> {";
+            for(auto iter:adj[i])
+                cout<<iter<<" ";
+            cout<<"}\n";
+        }
+        //cout<<"\n";
+        for(int i=0;i<n;++i)
+        {
+            for(auto iter:adj[i])
+                ++indeg[iter];
+        }
+        queue<int>q;
+        for(int i=0;i<n;++i)
+        {
+            if(indeg[i]==0)
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+            int node=q.front();
+            topo.emplace_back(node);
+            q.pop();
+            for(auto iter:adj[node])
+            {
+                --indeg[iter];
+                if(indeg[iter]==0)
+                    q.push(iter);
+            }
+        }
+        sort(topo.begin(),topo.end());
+        return topo;
+    }
+};
