8acacfeb7bc353fa729003205fe1b9c24ad879d6
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-08-26 11:54:08 +0530

    Time: 6 ms (99.29%) | Memory: 16.9 MB (90.70%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int n,vector<vector<int>>&pre) {
+        vector<int>adj[n],indeg(n,0),topo;
+        for(int i=0;i<pre.size();++i)
+            adj[pre[i][1]].emplace_back(pre[i][0]);
+        for(int i=0;i<n;++i)
+        {
+            for(auto iter:adj[i])
+                ++indeg[iter];
+        }
+        queue<int>q;
+        for(int i=0;i<n;++i)
+        {
+            if(indeg[i]==0)
+                q.push(i);
+        }
+        while(!q.empty())
+        {
+            int node=q.front();
+            topo.emplace_back(node);
+            q.pop();
+            for(auto iter:adj[node])
+            {
+                --indeg[iter];
+                if(indeg[iter]==0)
+                    q.push(iter);
+            }
+        }
+        return (topo.size()==n);
+    }
+};
