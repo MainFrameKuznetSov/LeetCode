3fe2f9ec0a7b8de1df6554c3bdfcd585c206bb23
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-09-02 09:00:24 +0530

    Time: 73 ms (95.57%) | Memory: 83.9 MB (5.65%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int chalkReplacer(vector<int>& chalk, int k) {
+        int n=chalk.size();
+        vector<long long>psum(n,0);
+        psum[0]=chalk[0];
+        for(int i=1;i<n;++i)
+            psum[i]=psum[i-1]+chalk[i];
+        k=k%psum[n-1];
+        for(int i=0;i<n;++i)
+        {
+            if(k<psum[i])
+                return i;
+            else if(k==psum[i])
+                return i+1;
+        }
+        return -1;
+    }
+};
