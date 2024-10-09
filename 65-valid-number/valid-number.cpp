af30a21b5fcc67c5e5c5ec6744508d70c7afbd14
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-10-09 12:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (43.73%) - LeetSync

---
+++ 65-valid-number/valid-number.cpp
@@ -0,0 +1,60 @@
+class Solution {
+private:
+    bool solve(string &s, int ind) 
+    {
+        if(ind<s.size())
+        {
+            if(s[ind]=='-' || s[ind]=='+') 
+                ++ind;
+        }
+        bool hasDigit=false;
+        while(ind<s.size()) 
+        {
+            char ch=s[ind];
+            if(!isdigit(ch)) 
+                return false;
+            hasDigit=true; 
+            ++ind;
+        }
+        return hasDigit;
+    }
+    
+public:
+    bool isNumber(string s) 
+    {
+        bool dec=false;
+        bool ans=false;  
+        int i=0;
+        if (s[i]=='-' || s[i]=='+') 
+            ++i;
+        while(i<s.size()) 
+        {
+            char ch=s[i];
+            if(ch=='-' || ch=='+') 
+                return false;
+            else if(isalpha(ch)) 
+            {
+                if (ch=='e' || ch=='E') 
+                {
+                    if(ans && solve(s,i+1)) 
+                        return true;
+                    else 
+                        return false;
+                } 
+                else 
+                    return false;
+            }
+            else if(ch == '.') 
+            {
+                if(dec) 
+                    return false;
+                else 
+                    dec=true;
+            }
+            else 
+                ans=true; 
+            ++i;
+        }
+        return ans;
+    }
+};
