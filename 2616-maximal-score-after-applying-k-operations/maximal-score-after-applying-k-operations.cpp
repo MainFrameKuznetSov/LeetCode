3ea45559e45d67a8098a2c9ad0fa991a3ba4b249
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-10-14 12:26:41 +0530

    Time: 192 ms (66.58%) | Memory: 79.8 MB (50.62%) - LeetSync

--- 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     long long maxKelements(vector<int>& nums, int k) {
         priority_queue<int,vector<int>>pq;
-        for(int it:nums)
-            pq.push(it);
-        long long ans=0;
-        while(k)
+        for(int i=0;i<nums.size();++i)
+            pq.push(nums[i]);
+        long long score=0;
+        while(k--)
         {
-            int val=pq.top();
-            ans+=val;
+            int temp=pq.top();
+            score+=temp;
             pq.pop();
-            int temp=val%3==0?val/3:val/3+1;
-            pq.push(temp);
-            //cout<<val<<" "<<temp<<"\n";
-            --k;
+            int newVal=(temp%3==0)?temp/3:temp/3+1;
+            /*if(temp%3==0)
+                newVal=temp/3;
+            else
+                newVal=temp/3+1;*/
+            pq.push(newVal);
         }
-        return ans;
+        return score;
     }
 };
