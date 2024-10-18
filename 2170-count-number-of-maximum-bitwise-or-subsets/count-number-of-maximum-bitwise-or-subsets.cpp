3f7fa20c61fe88facb3a0b10f8eac0273db6dde7
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-10-18 12:27:33 +0530

    Time: 124 ms (12.24%) | Memory: 139.5 MB (12.88%) - LeetSync

--- 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    void recur(vector<int>&nums,int ind,int curr,int mx,int &cnt)
-    {
-        if(curr==mx)
-            ++cnt;
-        for(int i=ind;i<nums.size();++i)
-            recur(nums,i+1,curr|nums[i],mx,cnt);
-    }
     int countMaxOrSubsets(vector<int>& nums) {
         int mx=0,cnt=0;
         for(int i:nums)
             mx|=i;
-        recur(nums,0,0,mx,cnt);
-        return cnt;
+        vector<int>dp(1<<17+1,0);
+        dp[0]=1;
+        for(int i:nums)
+        {
+            for(int msk=1<<17;msk>=0;--msk)
+            {
+                if(dp[msk]!=0)
+                    dp[msk|i]+=dp[msk];
+            }
+        }
+        return dp[mx];
     }
 };
