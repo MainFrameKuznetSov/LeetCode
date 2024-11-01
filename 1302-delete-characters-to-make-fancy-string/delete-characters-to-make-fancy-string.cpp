db19450bfe1a20259d74a81fd362e1c117caf697
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-11-01 12:07:55 +0530

    Time: 40 ms (20.78%) | Memory: 42.9 MB (10.70%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        if(s.size()<3)
+            return s;
+        string ans="";
+        ans+=s[0];
+        ans+=s[1];
+        for(int i=2;i<s.size();++i)
+        {
+            char c=s[i];
+            if(s[i-1]==c && s[i-2]==c)
+            {
+
+            }
+            else
+                ans+=c;
+        }
+        return ans;
+    }
+};
