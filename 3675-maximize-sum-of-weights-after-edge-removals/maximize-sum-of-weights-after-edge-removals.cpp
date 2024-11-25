58eeb815114c5b9114e3776aaae9e5ac2a44259e
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-11-25 23:30:46 +0530

    Time: 1062 ms (12.50%) | Memory: 417.4 MB (6.25%) - LeetSync

--- 3675-maximize-sum-of-weights-after-edge-removals/maximize-sum-of-weights-after-edge-removals.cpp
+++ 3675-maximize-sum-of-weights-after-edge-removals/maximize-sum-of-weights-after-edge-removals.cpp
@@ -1,7 +1,7 @@
 class Solution {
     void dfs(vector<vector<pair<int,int>>>&adj,int u,int prnt,int k,vector<vector<long long>>&dp) 
     {
-        vector<long long>differences;
+        vector<long long>diff;
         long long sumW=0;
         for (auto it:adj[u]) 
         {
@@ -10,25 +10,25 @@ class Solution {
 
             dfs(adj,it.first,u,k,dp);
 
-            differences.push_back(it.second+dp[it.first][1]-dp[it.first][0]);
+            diff.push_back(it.second+dp[it.first][1]-dp[it.first][0]);
 
             sumW+=dp[it.first][0];
         }
 
-        sort(rbegin(differences),rend(differences));
+        sort(rbegin(diff),rend(diff));
 
         dp[u][0]=sumW;
-        for (int i=0;i<min(k,(int)differences.size());++i) 
+        for(int i=0;i<min(k,(int)diff.size());++i) 
         {
-            if (differences[i]>0) 
-                dp[u][0]+=differences[i];
+            if(diff[i]>0) 
+                dp[u][0]+=diff[i];
         }
 
         dp[u][1]=sumW;
-        for(int i=0;i<min(k-1,(int)differences.size());++i) 
+        for(int i=0;i<min(k-1,(int)diff.size());++i) 
         {
-            if(differences[i]>0) 
-                dp[u][1]+=differences[i];
+            if(diff[i]>0) 
+                dp[u][1]+=diff[i];
         }
     }
 
@@ -45,9 +45,7 @@ public:
         }
 
         vector<vector<long long>>dp(n,vector<long long>(2,-1));
-
         dfs(adj,0,-1,k,dp);
-
         return dp[0][0];
     }
 };
