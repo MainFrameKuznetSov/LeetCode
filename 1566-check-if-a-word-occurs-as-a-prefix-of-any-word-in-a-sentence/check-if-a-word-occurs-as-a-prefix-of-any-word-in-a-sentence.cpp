4780605da2333ef72c2861def0f257a7b429d57f
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2024-12-02 11:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (6.42%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -3,23 +3,20 @@ public:
     int isPrefixOfWord(string sen, string word) {
         sen+=" ";
         string temp="";
-        int ans=0;
+        int ans=1;
         for(int i=0;i<sen.size();++i)
         {
             if(sen[i]==' ')
             {
                 ++ans;
-                string nx="";
-                for(int j=0;j<temp.size();++j)
-                {
-                    nx+=temp[j];
-                    if(nx==word)
-                        return ans;
-                }
                 temp="";
             }
             else
+            {
                 temp+=sen[i];
+                if(temp==word)
+                    return ans;
+            }
         }
         return -1;
     }
