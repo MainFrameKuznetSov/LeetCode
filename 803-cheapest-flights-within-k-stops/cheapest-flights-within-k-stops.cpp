960b5aada3fcbeb9de3f042bd6472a026edc1992
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-01-13 23:54:03 +0530

    Time: 14 ms (12.64%) | Memory: 21.5 MB (7.59%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int>dist(n,1e9);
+        dist[src]=0;
+        vector<vector<int>>adj[n];
+        for(auto it:flights)
+            adj[it[0]].push_back({it[1],it[2]});
+        set<pair<int,pair<int,int>>>st;
+        st.insert({0,{src,0}});
+        while(!st.empty())
+        {
+            auto top=*st.begin();
+            int stops=top.first;
+            int node=top.second.first;
+            int cost=top.second.second;
+            st.erase(top);
+            if(stops>k)
+                continue;
+            for(auto it:adj[node])
+            {
+                int nextNode=it[0],price=it[1];
+                //cout<<node<<" "<<cost<<" "<<nextNode<<" "<<price<<"\n";
+                if(cost+price<dist[nextNode] && stops<=k)
+                {
+                    dist[nextNode]=cost+price;
+                    st.insert({stops+1,{nextNode,dist[nextNode]}});
+                    //cout<<"Change "<<dist[nextNode]<<" "<<nextNode<<" "<<stops+1<<"\n";
+                }
+            }
+        }
+        // for(int i=0;i<n;++i)
+        //     cout<<dist[i]<<" ";
+        if(dist[dst]==1e9)
+            return -1;
+        return dist[dst];
+    }
+};
