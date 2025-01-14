bb239faafe5e230ff5b930515585118092c5c916
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-01-14 10:20:57 +0530

    Time: 14 ms (12.78%) | Memory: 21 MB (8.69%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -7,13 +7,13 @@ public:
         for(auto it:flights)
             adj[it[0]].push_back({it[1],it[2]});
         set<pair<int,pair<int,int>>>st;
-        st.insert({0,{src,0}});
+        st.insert({0,{0,src}});
         while(!st.empty())
         {
             auto top=*st.begin();
             int stops=top.first;
-            int node=top.second.first;
-            int cost=top.second.second;
+            int node=top.second.second;
+            int cost=top.second.first;
             st.erase(top);
             if(stops>k)
                 continue;
@@ -24,7 +24,7 @@ public:
                 if(cost+price<dist[nextNode] && stops<=k)
                 {
                     dist[nextNode]=cost+price;
-                    st.insert({stops+1,{nextNode,dist[nextNode]}});
+                    st.insert({stops+1,{dist[nextNode],nextNode}});
                     //cout<<"Change "<<dist[nextNode]<<" "<<nextNode<<" "<<stops+1<<"\n";
                 }
             }
