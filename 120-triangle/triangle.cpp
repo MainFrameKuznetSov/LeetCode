0dfb16bb022229307e6ddf76bd01426c896e04bb
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-01-24 19:49:58 +0530

    Time: 407 ms (4.44%) | Memory: 121 MB (5.69%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,21 +2,39 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int>prev(n,0);
+        vector<vector<int>>lim;
         for(int i=0;i<n;++i)
-            prev[i]=triangle[n-1][i];
-        for(int i=n-2;i>=0;--i)
         {
-            vector<int>curr(n,0);
-            for(int j=i;j>=0;--j)
-            {
-                int d=triangle[i][j],rd=triangle[i][j];
-                d+=prev[j];
-                rd+=prev[j+1];
-                curr[j]=min(d,rd);
-            }
-            prev=curr;
+            vector<int>l;
+            for(int j=0;j<=i;++j)
+                l.push_back(INT_MAX);
+            lim.push_back(l);
         }
-        return prev[0];
+        set<pair<int,pair<int,int>>>st;
+        lim[0][0]=triangle[0][0];
+        st.insert({lim[0][0],{0,0}});
+        vector<pair<int,int>>dir={{1,0},{1,1}};
+        while(!st.empty())
+        {
+            auto top=*st.begin();
+            st.erase(top);
+            int dis=top.first,r=top.second.first,c=top.second.second;
+            for(int i=0;i<2;++i)
+            {
+                int nr=r+dir[i].first,nc=c+dir[i].second;
+                if(nr>=0 && nr<n && nc>=0 && nc<triangle[nr].size())
+                {
+                    if(dis+triangle[nr][nc]<lim[nr][nc])
+                    {
+                        lim[nr][nc]=dis+triangle[nr][nc];
+                        st.insert({lim[nr][nc],{nr,nc}});
+                    }
+                }
+            }
+        }
+        int ans=INT_MAX;
+        for(int i=0;i<n;++i)
+            ans=min(ans,lim[n-1][i]);
+        return ans;
     }
 };
