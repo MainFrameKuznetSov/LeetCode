dfec6f8762809b3b75ff24da95c1b4578e2a07f6
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-02-03 21:16:23 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (64.71%) - LeetSync

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
         int n=nums.size(),ans=0;
-        for(int i=0;i<n;++i)
+        for(int i=0;i<n;++i)//O(n)
         {
-            for(int j=i;j<n;++j)
+            for(int j=i;j<n;++j)//O(n)
             {
                 int val=nums[j],len=1;
                 ans=max(ans,len);
                 bool dec=0,inc=0;
-                for(int k=j+1;k<n;++k)
+                for(int k=j+1;k<n;++k)//O(n)
                 {
                     if(k==j+1)
                     {
@@ -40,5 +40,9 @@ public:
             }
         }
         return ans;
+        /*
+            Overall Time complexity:- O(n*n*n)
+            Overall Space complexity:- O(1)
+        */
     }
 };
