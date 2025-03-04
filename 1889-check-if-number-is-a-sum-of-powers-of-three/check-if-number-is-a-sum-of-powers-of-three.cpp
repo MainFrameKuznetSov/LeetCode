c362f6b6aa32ff732a5eb321ed65da82a6f42236
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-03-04 19:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (41.55%) - LeetSync

--- 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        unordered_set<int>t;
         while(n)
         {
-            int fact=(int)(log2(n)/log2(3));
-            int diff=pow(3,fact);
-            //n-=pow(3,fact);
-            if(t.count(diff))
+            if(n%3==2)
                 return 0;
-            t.insert(diff);
-            n-=diff;
-            //cout<<diff<<" "<<n<<"\n";
+            n/=3;
         }
         return 1;
     }
