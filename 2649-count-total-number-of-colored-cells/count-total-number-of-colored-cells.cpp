30de9804a0e2b12ac34869a14cafbf3342bf6c83
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-03-05 09:47:42 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (61.42%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long ans=0,lim=(n*2)-1;
-        for(int i=1;i<=lim;i+=2)
-        {
-            if(i!=lim)
-                ans+=(i*2);
-            else
-                ans+=i;
-        }
-        return ans;
+        return 1+((long long)((long long)n*(n-1)*2));
     }
 };
