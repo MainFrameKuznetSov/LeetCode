745f618ca88abe22555ca55222cfb01490be0f9a
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-03-06 11:38:02 +0530

    Time: 14 ms (52.44%) | Memory: 31.8 MB (24.71%) - LeetSync

---
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n=grid.size(),miss=0,repeat=0;
+        unordered_map<int,int>mp;
+        for(int i=1;i<=(n*n);++i)
+        {
+            miss^=i;
+            ++mp[i];
+        }
+        //cout<<miss<<"\n";
+        for(int i=0;i<n;++i)
+        {
+            for(int j=0;j<n;++j)
+            {
+                // repeat.erase(grid[i][j]);
+                if(mp[grid[i][j]]==1)
+                {
+                    mp.erase(grid[i][j]);
+                    miss^=grid[i][j];
+                }
+                else 
+                {
+                    mp.erase(grid[i][j]);
+                    repeat=grid[i][j];
+                }
+            }
+        }
+        vector<int>ans;
+        ans.push_back(repeat);
+        ans.push_back(miss);
+        return ans;
+    }
+};
