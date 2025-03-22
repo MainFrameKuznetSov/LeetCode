9a0237d820416b2d9c0ed7687e29f0b7f8bb3696
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-03-22 21:25:25 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (58.62%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int>dist(n,1e9);
-        dist[src]=0;
-        vector<vector<int>>adj[n];
+        vector<pair<int,int>>adj[n];
         for(auto it:flights)
             adj[it[0]].push_back({it[1],it[2]});
         set<pair<int,pair<int,int>>>st;
         st.insert({0,{0,src}});
+        vector<int>dist(n,1e9);
+        dist[src]=0;
         while(!st.empty())
         {
-            auto top=*st.begin();
-            int stops=top.first;
-            int node=top.second.second;
-            int cost=top.second.first;
-            st.erase(top);
-            if(stops>k)
-                continue;
+            auto curr=*st.begin();
+            st.erase(curr);
+            int stops=curr.first;
+            int node=curr.second.second,cost=curr.second.first;
             for(auto it:adj[node])
             {
-                int nextNode=it[0],price=it[1];
-                //cout<<node<<" "<<cost<<" "<<nextNode<<" "<<price<<"\n";
+                int nextNode=it.first,price=it.second;
                 if(cost+price<dist[nextNode] && stops<=k)
                 {
                     dist[nextNode]=cost+price;
                     st.insert({stops+1,{dist[nextNode],nextNode}});
-                    //cout<<"Change "<<dist[nextNode]<<" "<<nextNode<<" "<<stops+1<<"\n";
                 }
             }
         }
-        // for(int i=0;i<n;++i)
-        //     cout<<dist[i]<<" ";
         if(dist[dst]==1e9)
             return -1;
         return dist[dst];
