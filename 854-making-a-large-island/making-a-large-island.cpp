a6a7f620bb31f17d1e124bca639a76f8f6435d4c
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-03-23 19:56:21 +0530

    Time: 283 ms (46.90%) | Memory: 159.6 MB (38.81%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -1,45 +1,54 @@
-class DisjointSetUnion
+class DisjointSet
 {
-public:
-    vector<int>prnt,size;
-    DisjointSetUnion(int n)
-    {
-        prnt.resize(n+1,0);
-        size.resize(n+1,1);
-        for(int i=0;i<=n;++i)
-            prnt[i]=i;
-    }
-    int findUPar(int u)
-    {
-        if(prnt[u]==u)
-            return u;
-        else
+    public:
+        vector<int>prnt,size;
+        int components;
+        DisjointSet(int n)
+        {
+            components=n;
+            prnt.resize(n,-1);
+            size.resize(n,1);
+            for(int i=0;i<n;++i)
+                prnt[i]=i;
+        }
+
+        int getComponents()
+        {
+            return components;
+        }
+
+        int findUPar(int u)
+        {
+            if(prnt[u]==u)
+                return u;
             return prnt[u]=findUPar(prnt[u]);
-    }
-    void UnionBySize(int u,int v)
-    {
-        int ulp_u=findUPar(u);
-        int ulp_v=findUPar(v);
-        if(ulp_u==ulp_v)
-            return ;
-        if(size[ulp_u]>size[ulp_v])
-        {
-            prnt[ulp_v]=ulp_u;
-            size[ulp_u]+=size[ulp_v];
         }
-        else
+
+        void unionBySize(int u,int v)
         {
-            prnt[ulp_u]=ulp_v;
-            size[ulp_v]+=size[ulp_u];
+            int ulp_u=findUPar(u),ulp_v=findUPar(v);
+            if(ulp_u==ulp_v)
+                return ;
+            if(size[ulp_u]>size[ulp_v])
+            {
+                size[ulp_u]+=size[ulp_v];
+                prnt[ulp_v]=ulp_u;
+                --components;
+            }
+            else
+            {
+                size[ulp_v]+=size[ulp_u];
+                prnt[ulp_u]=ulp_v;
+                --components;
+            }
         }
-    }
 };
 
 class Solution {
 public:
     int largestIsland(vector<vector<int>>& grid) {
         int n=grid.size();
-        DisjointSetUnion dsu(n*n);
+        DisjointSet dsu(n*n);
         vector<pair<int,int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
         for(int i=0;i<n;++i)
         {
@@ -50,8 +59,8 @@ public:
                 for(int k=0;k<4;++k)
                 {
                     int ni=i+dir[k].first,nj=j+dir[k].second;
-                    if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==1)
-                        dsu.UnionBySize(i*n+j,ni*n+nj);
+                    if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj])
+                        dsu.unionBySize(i*n+j,ni*n+nj);
                 }
             }
         }
@@ -60,26 +69,28 @@ public:
         {
             for(int j=0;j<n;++j)
             {
+                if(grid[i][j])
+                    continue;
                 unordered_set<int>st;
-                if(grid[i][j]==0)
+                for(int k=0;k<4;++k)
                 {
-                    for(int k=0;k<4;++k)
+                    int ni=i+dir[k].first,nj=j+dir[k].second;
+                    if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj])
                     {
-                        int ni=i+dir[k].first,nj=j+dir[k].second;
-                        if(ni>=0 && ni<n && nj>=0 && nj<n && grid[ni][nj]==1)
-                            st.insert(dsu.findUPar(ni*n+nj));
+                        st.insert(dsu.findUPar(ni*n+nj));
+                        //cout<<ni<<" "<<nj<<"\n";
                     }
                 }
-                else
-                    continue;
-                int temp=0;
+                int newSize=1;
                 for(auto it:st)
-                    temp+=dsu.size[it];
-                ans=max(ans,temp+1);
+                    newSize+=dsu.size[it];
+                //cout<<newSize<<"\n";
+                ans=max(ans,newSize);
+                st.clear();
             }
         }
         for(int i=0;i<n*n;++i)
-            ans=max(ans,dsu.size[dsu.findUPar(i)]);
+            ans=max(ans,dsu.size[dsu.prnt[i]]);
         return ans;
     }
 };
