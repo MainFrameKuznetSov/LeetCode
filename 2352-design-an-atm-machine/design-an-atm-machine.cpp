3b6117b70748d0b5919ded7ad40c0af0cfa7487f
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-05-12 13:07:19 +0530

    Time: 23 ms (23.40%) | Memory: 109.6 MB (32.08%) - LeetSync

---
+++ 2352-design-an-atm-machine/design-an-atm-machine.cpp
@@ -0,0 +1,58 @@
+class ATM {
+public:
+
+    long long tot;
+    vector<int>curr,val;
+
+    ATM() {
+        tot=0LL;
+        curr.assign(5,0);
+        val={20,50,100,200,500};
+    }
+    
+    void deposit(vector<int> notes) {
+        for(int i=0;i<5;++i)
+        {
+            curr[i]+=notes[i];
+            tot+=(1LL*val[i]*notes[i]);
+        }
+        // for(int i:curr)
+        //     cout<<i<<" ";
+        // cout<<"\n"<<tot<<"\n";
+        return ;
+    }
+    
+    vector<int> withdraw(int amount) {
+        vector<int>ans(5);
+        if(amount>tot)
+            return {-1};
+        int pre=amount;
+        vector<int>temp=curr;
+        for(int i=4;i>=0;--i)
+        {
+            //cout<<val[i]<<"\n";
+            if(amount>=val[i])
+            {
+                int cnt=(amount/val[i]);
+                cnt=min(cnt,curr[i]);
+                amount-=(cnt*val[i]);
+                curr[i]-=cnt;
+                ans[i]+=cnt;
+            }
+        }
+        if(amount)
+        {
+            curr=temp;
+            return {-1};
+        }
+        tot-=pre;
+        return ans;
+    }
+};
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM* obj = new ATM();
+ * obj->deposit(banknotesCount);
+ * vector<int> param_2 = obj->withdraw(amount);
+ */
