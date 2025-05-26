347feb6a5b5c1e3bd6759eca2cd4825de98f73b2
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-05-26 23:14:42 +0530

    Time: 107 ms (9.62%) | Memory: 105.3 MB (11.14%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.cpp
@@ -0,0 +1,47 @@
+class BrowserHistory {
+public:
+
+    string home;
+    stack<string>bwd,fwd;
+
+    BrowserHistory(string homepage) {
+        bwd.push(homepage);
+        this->home=homepage;
+    }
+    
+    void visit(string url) {
+        while(!fwd.empty())
+            fwd.pop();
+        bwd.push(url);
+    }
+    
+    string back(int steps) {
+        while(steps && bwd.size()>1)
+        {
+            string node=bwd.top();
+            bwd.pop();
+            fwd.push(node);
+            --steps;
+        }
+        return bwd.empty()?home:bwd.top();
+    }
+    
+    string forward(int steps) {
+        while(steps && !fwd.empty())
+        {
+            string node=fwd.top();
+            fwd.pop();
+            bwd.push(node);
+            --steps;
+        }
+        return bwd.empty()?home:bwd.top();
+    }
+};
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory* obj = new BrowserHistory(homepage);
+ * obj->visit(url);
+ * string param_2 = obj->back(steps);
+ * string param_3 = obj->forward(steps);
+ */
