e373defb25d0416035df2e539b3c2b3aaf52df24
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-06-18 18:38:58 +0530

    Time: 56 ms (30.86%) | Memory: 120.2 MB (44.50%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -4,11 +4,11 @@ public:
         vector<vector<int>>ans;
         sort(nums.begin(),nums.end());
         int n=nums.size();
-        for(int i=2;i<n;i+=3)
+        for(int i=0;i<n;i+=3)
         {
-            if(nums[i]>k+nums[i-2])
+            if(nums[i+2]>k+nums[i])
                 return {};
-            ans.push_back({nums[i],nums[i-1],nums[i-2]});            
+            ans.push_back({nums[i],nums[i+1],nums[i+2]});            
         }
         return ans;
     }
