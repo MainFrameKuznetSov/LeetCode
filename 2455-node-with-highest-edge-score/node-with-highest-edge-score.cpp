38ca383a3dc72257fd4c52b80e7d8b5984ebad8d
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-08 12:41:48 +0530

    Time: 7 ms (72.05%) | Memory: 124.1 MB (43.48%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int edgeScore(vector<int>& edges) {
-        int n=edges.size();
-        long long mx=0,ans=-1;
-        vector<int>adj[n];
+        int n=edges.size(),ans=0;
+        vector<long long>sum(n,0);
         for(int i=0;i<n;++i)
-            adj[edges[i]].push_back(i);
-        for(int i=0;i<n;++i)
+            sum[edges[i]]+=i;
+        for(int i=1;i<n;++i)
         {
-            long long sum=0;
-            for(auto it:adj[i])
-                sum+=it;
-            //cout<<i<<" "<<sum<<" "<<mx<<"\n";
-            if(sum>mx)
-            {
-                mx=sum;
+            if(sum[i]>sum[ans])
                 ans=i;
-            }
         }
-        return (int)ans;
+        return ans;
     }
 };
