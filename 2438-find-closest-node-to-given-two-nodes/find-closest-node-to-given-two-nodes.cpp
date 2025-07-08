31c01e703c8bcbca95cdcad846cdf0a42219bd00
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-08 12:29:20 +0530

    Time: 31 ms (24.10%) | Memory: 107.9 MB (28.46%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,47 +1,44 @@
 class Solution {
 public:
 
-    vector<int> distance(int node,vector<int>adj[],int n)
+    vector<int>distance(vector<int>&edges,int node)
     {
+        int n=edges.size();
         vector<int>dist(n,INT_MAX);
         dist[node]=0;
         queue<pair<int,int>>q;
         q.push({0,node});
         while(!q.empty())
         {
-            int dis=q.front().first,node=q.front().second;
+            int dis=q.front().first,nd=q.front().second;
             q.pop();
-            for(auto it:adj[node])
+            int it=edges[nd];
+            if(it==-1)
+                continue;
+            if(dis+1<dist[it])
             {
-                int nextNode=it;
-                if(dis+1<dist[nextNode])
-                {
-                    dist[nextNode]=dis+1;
-                    q.push({dis+1,nextNode});
-                }
+                dist[it]=dis+1;
+                q.push({dis+1,it});
             }
         }
         return dist;
     }
 
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n=edges.size();
-        vector<int>adj[n];
+        vector<int>v1=distance(edges,node1),v2=distance(edges,node2);
+        int ans=INT_MAX,maxDist=INT_MAX,n=edges.size();
         for(int i=0;i<n;++i)
         {
-            if(edges[i]!=-1)
-                adj[i].push_back(edges[i]);
-        }
-        vector<int>v1=distance(node1,adj,n),v2=distance(node2,adj,n);
-        int ans=-1,minDist=INT_MAX;
-        for(int i=0;i<n;++i)
-        {
-            if(v1[i]!=INT_MAX && v2[i]!=INT_MAX && max(v1[i],v2[i])<minDist)
+            if(v1[i]!=INT_MAX && v2[i]!=INT_MAX)
             {
-                minDist=max(v1[i],v2[i]);
-                ans=i;
+                int temp=max(v1[i],v2[i]);
+                if(temp<maxDist)
+                {
+                    maxDist=temp;
+                    ans=i;
+                }
             }
         }
-        return ans;
+        return ans==INT_MAX?-1:ans;
     }
 };
