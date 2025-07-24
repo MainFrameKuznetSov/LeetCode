6e5d8451a4c6258eafcdf85b6ea6ecbde6fb15c5
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-24 20:33:17 +0530

    Time: 450 ms (23.99%) | Memory: 225.3 MB (17.96%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    int f(int ind,int buy,int count,vector<int>&prices,vector<vector<vector<int>>>&dp)
+    int f(int ind,int buy,int count,vector<int>&prices,vector<vector<vector<int>>>&dp,int k)
     {
         if(ind==prices.size()-1)
         {
@@ -10,7 +10,7 @@ public:
             return prices[ind];
         }
 
-        if(count>1)
+        if(count>k-1)
             return 0;
 
         if(dp[ind][buy][count]!=-1)
@@ -19,14 +19,14 @@ public:
         int Buy=0,notBuy=0;
         if(buy)
         {
-            int notTake=f(ind+1,buy,count,prices,dp);
-            int Take=-prices[ind]+f(ind+1,1-buy,count,prices,dp);
+            int notTake=f(ind+1,buy,count,prices,dp,k);
+            int Take=-prices[ind]+f(ind+1,1-buy,count,prices,dp,k);
             Buy=max(Take,notTake);
         }
         else
         {
-            int notTake=f(ind+1,buy,count,prices,dp);
-            int Take=prices[ind]+f(ind+1,1-buy,count+1,prices,dp);
+            int notTake=f(ind+1,buy,count,prices,dp,k);
+            int Take=prices[ind]+f(ind+1,1-buy,count+1,prices,dp,k);
             notBuy=max(Take,notTake);
         }
 
@@ -35,8 +35,8 @@ public:
     }
 
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(2,-1)));
-        return f(0,1,0,prices,dp);
+        int n=prices.size(),k=2;
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k,-1)));
+        return f(0,1,0,prices,dp,k);
     }
 };
