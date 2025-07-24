c623cd8e3edcff057606e8c2dfaebaa654b71def
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-24 20:43:16 +0530

    Time: 899 ms (5.04%) | Memory: 354.7 MB (5.70%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -37,15 +37,17 @@ public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size(),k=2;
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(k,-1)));
+        vector<vector<int>>prev(2,vector<int>(k+1,0));
         //return f(0,1,0,prices,dp,k);
         for(int buy=0;buy<=1;++buy)
         {
             for(int count=0;count<k;++count)
-                dp[n-1][buy][count]=buy?0:prices[n-1];
+                prev[buy][count]=buy?0:prices[n-1];
         }
 
         for(int i=n-2;i>=0;--i)
         {
+            vector<vector<int>>curr(2,vector<int>(k+1,0));
             for(int buy=0;buy<=1;++buy)
             {
                 for(int count=0;count<k;++count)
@@ -53,23 +55,24 @@ public:
                     int Buy=0,notBuy=0;
                     if(buy)
                     {
-                        int notTake=f(i+1,buy,count,prices,dp,k);
-                        int Take=-prices[i]+f(i+1,1-buy,count,prices,dp,k);
+                        int notTake=prev[buy][count];
+                        int Take=-prices[i]+prev[1-buy][count];
                         Buy=max(Take,notTake);
                     }
                     else
                     {
-                        int notTake=f(i+1,buy,count,prices,dp,k);
-                        int Take=prices[i]+f(i+1,1-buy,count+1,prices,dp,k);
+                        int notTake=prev[buy][count],Take=0;
+                        Take=prices[i]+prev[1-buy][count+1];
                         notBuy=max(Take,notTake);
                     }
 
-                    dp[i][buy][count]=max(Buy,notBuy);
+                    curr[buy][count]=max(Buy,notBuy);
                 }
             }
+            prev=curr;
         }
 
-        return dp[0][1][0];
+        return prev[1][0];
 
     }
 };
