7d243b2926129dc6f5c6d5962833c184a1394bc2
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-24 22:03:45 +0530

    Time: 7 ms (8.78%) | Memory: 16.2 MB (35.27%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -31,28 +31,31 @@ public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n+2,vector<int>(2,0));
+        vector<int>prev(2,0),curr1(2,0);
         //return f(0,1,prices,dp);
         for(int ind=n-1;ind>=0;--ind)
         {
+            vector<int>curr2(2,0);
             for(int canBuy=0;canBuy<=1;++canBuy)
             {
                 int Buy=0,notBuy=0;
                 if(canBuy)
                 {
-                    int notTake=dp[ind+1][canBuy];
-                    int Take=-prices[ind]+dp[ind+1][1-canBuy];
+                    int notTake=curr1[canBuy];
+                    int Take=-prices[ind]+curr1[1-canBuy];
                     Buy=max(notTake,Take);
                 }
                 else
                 {
-                    int notTake=dp[ind+1][canBuy];
-                    int Take=prices[ind]+dp[ind+2][1-canBuy];
+                    int notTake=curr1[canBuy];
+                    int Take=prices[ind]+prev[1-canBuy];
                     notBuy=max(notTake,Take);
                 }
-                
-                dp[ind][canBuy]=max(Buy,notBuy);
+                curr2[canBuy]=max(Buy,notBuy);
             }
+            prev=curr1;
+            curr1=curr2;
         }
-        return dp[0][1];
+        return curr1[1];
     }
 };
