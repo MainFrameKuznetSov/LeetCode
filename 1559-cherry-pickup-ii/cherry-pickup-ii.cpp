89c340a23a520ecd4ada65a563c1c20d44b04ae5
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-24 12:30:11 +0530

    Time: 47 ms (69.26%) | Memory: 21 MB (53.03%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,38 +1,43 @@
+#define MIN -1e9
+
 class Solution {
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
-        int m=grid.size(),n=grid[0].size();
-        vector<vector<int>>prev(n,vector<int>(n,0));
-        for(int y1=0;y1<n;++y1)
+
+    int f(int x,int y1,int y2,vector<vector<int>>&mat,vector<vector<vector<int>>>&dp,int n,int m)
+    {
+        if(y1<0 || y2<0 ||  y1>=m || y2>=m)
+            return MIN;
+
+        if(dp[x][y1][y2]!=-1)
+            return dp[x][y1][y2];
+
+        if(x==n-1)
         {
-            for(int y2=0;y2<n;++y2)
-                prev[y1][y2]=(y1==y2)?grid[m-1][y1]:grid[m-1][y1]+grid[m-1][y2];
+            if(y1==y2)
+                return mat[x][y1];
+            else
+                return mat[x][y1]+mat[x][y2];
         }
-        for(int i=m-2;i>=0;--i)
+
+        int mx=MIN;
+        for(int i=-1;i<=1;++i)
         {
-            vector<vector<int>>curr(n,vector<int>(n,0));
-            for(int y1=0;y1<n;++y1)
+            for(int j=-1;j<=1;++j)
             {
-                for(int y2=0;y2<n;++y2)
-                {
-                    int maxi=INT_MIN;
-                    for(int dy1=1;dy1>=-1;--dy1)
-                    {
-                        for(int dy2=1;dy2>=-1;--dy2)
-                        {
-                            int temp=(y1==y2)?grid[i][y1]:grid[i][y1]+grid[i][y2];
-                            if(y1+dy1>=0 && y1+dy1<n && y2+dy2>=0 && y2+dy2<n)
-                                temp+=prev[y1+dy1][y2+dy2];
-                            else
-                                temp=INT_MIN;
-                            maxi=max(maxi,temp);
-                        }
-                    }
-                    curr[y1][y2]=maxi;
-                }
+                if(y1==y2)
+                    mx=max(mx,mat[x][y1]+f(x+1,y1+i,y2+j,mat,dp,n,m));
+                else
+                    mx=max(mx,mat[x][y1]+mat[x][y2]+f(x+1,y1+i,y2+j,mat,dp,n,m));
             }
-            prev=curr;
         }
-        return prev[0][n-1];
+
+        return dp[x][y1][y2]=mx;
+        
+    }
+
+    int cherryPickup(vector<vector<int>>& grid) {
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
+        return f(0,0,m-1,grid,dp,n,m);
     }
 };
