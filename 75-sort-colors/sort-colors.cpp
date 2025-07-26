269eb110f45cdaf0f0c5670dce093273abbe6c3e
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-26 09:08:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (78.92%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int c0=0,c1=0,c2=0,n=nums.size(),cur=0;
+        int n=nums.size();
+        int l=0,mid=0,r=n-1;
         for(int i=0;i<n;++i)
         {
-            if(nums[i]==0)
-                ++c0;
-            else if(nums[i]==1)
-                ++c1;
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[l]);
+                ++mid;
+                ++l;
+            }
+            else if(nums[mid]==1)
+                ++mid;
             else
-                ++c2;
+            {
+                swap(nums[mid],nums[r]);
+                --r;
+            }
         }
-        for(int i=1;i<=c0;++i)
-            nums[cur++]=0;
-        for(int i=1;i<=c1;++i)
-            nums[cur++]=1;
-        for(int i=1;i<=c2;++i)
-            nums[cur++]=2;
     }
 };
