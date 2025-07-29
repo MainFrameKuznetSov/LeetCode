01e4a04b1c125ddebd1a7be002b488884552d4c9
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-29 11:42:22 +0530

    Time: 87 ms (92.60%) | Memory: 14 MB (6.61%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -19,10 +19,26 @@ public:
     }
 
     int maxCoins(vector<int>& nums) {
+        int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        return f(1,n-2,nums,dp);
+        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
+        //return f(1,<n-2,nums,dp);
+        for(int i=n;i>=1;--i)
+        {
+            for(int j=1;j<=n;++j)
+            {
+                if(i>j)
+                    continue;
+                int mx=INT_MIN;
+                for(int k=i;k<=j;++k)
+                {
+                    int cost=nums[i-1]*nums[k]*nums[j+1]+dp[i][k-1]+dp[k+1][j];
+                    mx=max(cost,mx);
+                }
+                dp[i][j]=mx;
+            }
+        }
+        return dp[1][n];
     }
 };
