5e160022d06690fb6c110d50c9e8a5c3c590c8a7
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-29 11:32:53 +0530

    Time: 207 ms (21.24%) | Memory: 14 MB (6.61%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
+
+    long long f(int i,int j,vector<int>&nums,vector<vector<int>>&dp)
+    {
+        if(i>j)
+            return 0;
+
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+
+        long long mn=INT_MIN;
+        for(int k=i;k<=j;++k)
+        {
+            long long cost=nums[i-1]*nums[k]*nums[j+1]+f(i,k-1,nums,dp)+f(k+1,j,nums,dp);
+            mn=max(mn,cost);
+        }
+        return dp[i][j]=mn;
+    }
+
     int maxCoins(vector<int>& nums) {
-        int n=nums.size();
         nums.insert(nums.begin(),1);
         nums.push_back(1);
-        vector<vector<int>>dp(n+2,vector<int>(n+2,0));
-        for(int i=n;i>=1;--i)
-        {
-            for(int j=1;j<=n;++j)
-            {
-                if(i>j)
-                    continue;
-                int mx=INT_MIN;
-                for(int k=i;k<=j;++k)
-                {
-                    int coin=nums[i-1]*nums[k]*nums[j+1];
-                    int rem=dp[i][k-1]+dp[k+1][j];
-                    mx=max(coin+rem,mx);
-                }
-                dp[i][j]=mx;
-            }
-        }
-        return dp[1][n];
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return f(1,n-2,nums,dp);
     }
 };
