88b9936359f57f134569ab15cf53ab122282b5e3
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-31 14:12:31 +0530

    Time: 7 ms (7.13%) | Memory: 20.3 MB (5.24%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,62 +1,45 @@
 class Solution {
-
-private:
-    bool check(int start,vector<int>&color,vector<vector<int>>&graph)
-    {
-        queue<int>q;
-        q.push(start);
-        color[start]=0;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(auto it:graph[node])
-            {
-                if(color[it]==-1)
-                {
-                    color[it]=1-color[node];
-                    //col=1-col;
-                    q.push(it);
-                }
-                else
-                {
-                    if(color[it]==color[node])
-                        return 0;
-                }
-                //q.push(it);
-            }
-        }
-        return 1;
-    }
-    
-    bool dfs(int start,int col,vector<int>&color,vector<vector<int>>&graph)
-    {
-        color[start]=col;
-        for(auto it:graph[start])
-        {
-            if(color[it]==-1)
-            {
-                if(!dfs(it,1-col,color,graph))
-                    return 0;
-            }
-            else if(color[it]==color[start])
-                return 0;
-        }
-        return 1;
-    }
-
 public:
+
+    bool dfs(int node,vector<bool>&vis,vector<int>&color,vector<vector<int>>&adj,int col)
+    {
+        vis[node]=true;
+        color[node]=col;
+        for(auto it:adj[node])
+        {
+            if(!vis[it])
+            {
+                if(!dfs(it,vis,color,adj,1-col))
+                    return false;
+            }
+            else
+            {
+                if(color[it]==color[node])
+                    return false;
+                //return true;
+            }
+        }
+        return true;
+    }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n=graph.size();
-        vector<int>color(n,-1);
+        vector<vector<int>>adj(n);
         for(int i=0;i<n;++i)
         {
-            if(color[i]==-1)
+            for(auto it:graph[i])
+                adj[i].push_back(it);
+        }
+        vector<int>color(n,0);
+        vector<bool>vis(n,false);
+        for(int i=0;i<n;++i)
+        {
+            if(!vis[i])
             {
-                if(!dfs(i,0,color,graph))
-                    return 0;
+                if(!dfs(i,vis,color,adj,color[i]))
+                    return false;
             }
         }
-        return 1;
+        return true;
     }
 };
