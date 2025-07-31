8ad1aac22f7bbe4bc295e8dfd4f1e230b95d924a
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-07-31 12:34:20 +0530

    Time: 26 ms (30.47%) | Memory: 17.3 MB (84.57%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
         sort(nums.begin(),nums.end());
         vector<vector<int>>ans;
-        int n=nums.size();
         for(int i=0;i<n;++i)
         {
             if(i>0 && nums[i]==nums[i-1])
                 continue;
             for(int j=i+1;j<n;++j)
             {
-                if(j-1>i && nums[j]==nums[j-1])
+                if(j>i+1 && nums[j]==nums[j-1])
                     continue;
                 int k=j+1,l=n-1;
                 while(k<l)
@@ -19,10 +19,10 @@ public:
                     sum+=nums[j];
                     sum+=nums[k];
                     sum+=nums[l];
-                    if(sum<target)
-                        ++k;
-                    else if(sum>target)
+                    if(sum>target)
                         --l;
+                    else if(sum<target)
+                        ++k;
                     else
                     {
                         vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
