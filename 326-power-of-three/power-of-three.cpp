25dddd45272ed94671ef3974948defc6bf3d7193
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-08-13 14:42:38 +0530

    Time: 5 ms (32.93%) | Memory: 9.2 MB (5.34%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if(n<0)
             return 0;
-        double pwr=(floor(log10(n)/log10(3)));
+        double pwr=(floor(log2(n)/log2(3)));
         // cout<<pwr<<"\n";
         int val=(int)pow(3,pwr);
         // cout<<val<<"\n";
