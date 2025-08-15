d76c095c9e067568b00ddafd8b3d4eda428f86ba
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-08-15 15:33:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.79%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfFour(int n) {
         if(n<=0)
             return false;
-        double pwr=floor(log(n)/log(4));
+        double pwr=floor(log2(n)/log2(4));
         return n==pow(4,pwr);
     }
 };
