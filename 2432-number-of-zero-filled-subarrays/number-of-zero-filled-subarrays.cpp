1a465a2b0583328b175a5af74ff252db5aea06c6
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-08-19 12:17:42 +0530

    Time: 0 ms (100.00%) | Memory: 111.3 MB (74.50%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        long long ans=0,cnt=0LL;
+        int n=nums.size();
+        for(int i=0;i<n;++i)
+        {
+            if(nums[i]==0)
+                ++cnt;
+            else
+            {
+                ans+=(cnt*(cnt+1))/2;
+                cnt=0;
+            }
+        }
+        ans+=(cnt*(cnt+1))/2;
+        return ans;
+    }
+};
