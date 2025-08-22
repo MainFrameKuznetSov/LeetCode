6b524c1362d5dbc0700fc60282f665c9503916a2
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-08-22 14:49:39 +0530

    Time: 51 ms (25.74%) | Memory: 48.8 MB (43.97%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -26,8 +26,23 @@ public:
         if(amount==0)
             return 0;
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        int ans=f(n-1,amount,coins,dp);
-        return ans==1e9?-1:ans;
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        // int ans=f(n-1,amount,coins,dp);
+        // return ans==1e9?-1:ans;
+        for(int i=1;i<=amount;++i)
+            dp[0][i]=(i%coins[0]==0)?i/coins[0]:1e9;
+        for(int i=1;i<n;++i)
+        {
+            for(int j=1;j<=amount;++j)
+            {
+                int notTake=dp[i-1][j],Take=INT_MAX;
+                if(j>=coins[i])
+                    Take=1+dp[i][j-coins[i]];
+                dp[i][j]=min(notTake,Take);
+            }
+        }
+        if(dp[n-1][amount]==1e9)
+            return -1;
+        return dp[n-1][amount];
     }
 };
