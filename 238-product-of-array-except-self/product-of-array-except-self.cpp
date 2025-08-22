5097ca196a1fbfb10e801e1074ae86122eee5640
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-08-22 12:29:01 +0530

    Time: 843 ms (5.39%) | Memory: 42.3 MB (8.28%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>pprod(n+1,1),sprod(n+1,1),ans(n,1);
+        for(int i=0;i<n;++i)
+            pprod[i+1]=pprod[i]*nums[i];
+        for(int i=n-1;i>=0;--i)
+            sprod[i]=sprod[i+1]*nums[i];
+        for(int i:pprod)
+            cout<<i<<" ";
+        cout<<"\n";
+        for(int i:sprod)
+            cout<<i<<" ";
+        cout<<"\n";
+        for(int i=0;i<n;++i)
+            ans[i]=pprod[i]*sprod[i+1];
+        return ans;
+    }
+};
