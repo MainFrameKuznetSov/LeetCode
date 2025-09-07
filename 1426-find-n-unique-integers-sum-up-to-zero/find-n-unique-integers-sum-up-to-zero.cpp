6fd2df37e24a376d8fab6e9e58afb60449acd54c
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-09-07 13:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (83.69%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.cpp
@@ -5,22 +5,20 @@ public:
         if(n&1)
         {
             ans.push_back(0);
-            int lim=1;
+            int lim=0;
             for(int i=1;i<=n-1;i+=2)
             {
-                ans.push_back(lim);
+                ans.push_back(++lim);
                 ans.push_back(-lim);
-                ++lim;
             }
         }
         else
         {
-            int lim=1;
+            int lim=0;
             for(int i=1;i<=n-1;i+=2)
             {
-                ans.push_back(lim);
+                ans.push_back(++lim);
                 ans.push_back(-lim);
-                ++lim;
             }
         }
         return ans;
