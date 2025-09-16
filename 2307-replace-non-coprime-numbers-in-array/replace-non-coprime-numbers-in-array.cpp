a5f6ee1fff8ef765f2818505b2e89090a112de66
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-09-16 16:13:33 +0530

    Time: 12 ms (90.28%) | Memory: 125.1 MB (64.81%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>ans;
+        for(int it:nums)
+        {
+            while(!ans.empty())
+            {
+                int pre=ans.back();
+                int curr=it;
+                int GCD=__gcd(pre,curr);
+                if(GCD==1)
+                    break;
+                ans.pop_back();
+                int LCM=(pre*1LL*curr)/GCD;
+                it=LCM;
+            }
+            ans.push_back(it);
+        }
+        return ans;
+    }
+};
