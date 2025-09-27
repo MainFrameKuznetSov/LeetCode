48206f503c22504c5b1955f12a030a745118b45f
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-09-27 11:52:25 +0530

    Time: 3 ms (35.26%) | Memory: 10.4 MB (63.36%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -9,9 +9,9 @@ public:
     double largestTriangleArea(vector<vector<int>>& v) {
         double maxArea=0.0;
         int n=v.size();
-        for(int i=0;i<n;++i)
+        for(int i=0;i<n-2;++i)
         {
-            for(int j=i+1;j<n;++j)
+            for(int j=i+1;j<n-1;++j)
             {
                 for(int k=j+1;k<n;++k)
                     maxArea=max(maxArea,distance(v[i],v[j],v[k]));
