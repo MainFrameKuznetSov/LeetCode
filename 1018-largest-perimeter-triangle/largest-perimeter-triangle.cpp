9bc864e9cdc0ce0e77f22cc63059457c56ccfc0b
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-09-28 11:35:21 +0530

    Time: 5 ms (57.48%) | Memory: 25.7 MB (46.12%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -7,7 +7,7 @@ public:
     }
 
     int largestPerimeter(vector<int>& nums) {
-        int n=nums.size(),maxPeri=0;
+        int n=nums.size();
         sort(nums.begin(),nums.end());
         for(int i=n-1;i>=2;--i)
         {
