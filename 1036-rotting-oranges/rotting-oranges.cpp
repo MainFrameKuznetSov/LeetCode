98c1a5af2a4dd233ec2f91b1d2ec7c5f77a65049
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-11-12 20:14:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (19.97%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,40 +1,46 @@
 class Solution {
 public:
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size(),m=grid[0].size(),ans=0;
-        queue<pair<pair<int,int>,int>>q;
-        vector<vector<bool>>vis(n,vector<bool>(m,0));
-        for(int i=0;i<n;++i)
+    int orangesRotting(vector<vector<int>>&grid) {
+        int m=grid.size(),n=grid[0].size();
+        vector<vector<bool>>vis(m,vector<bool>(n,0));
+        queue<pair<int,pair<int,int>>>q;//{time,{row,col}}
+        for(int i=0;i<m;++i)
         {
-            for(int j=0;j<m;++j)
+            for(int j=0;j<n;++j)
             {
                 if(grid[i][j]==2)
                 {
-                    q.push({{i,j},0});
+                    q.push({0,{i,j}});
                     vis[i][j]=1;
                 }
             }
         }
+
         vector<pair<int,int>>dir={{-1,0},{0,1},{1,0},{0,-1}};
+        int ans=0;
+
         while(!q.empty())
         {
-            int i=q.front().first.first,j=q.front().first.second,time=q.front().second;
+            auto node=q.front();
             q.pop();
+            int time=node.first,row=node.second.first,col=node.second.second;
             ans=time;
             for(int k=0;k<4;++k)
             {
-                int ni=i+dir[k].first,nj=j+dir[k].second;
-                if(ni>=0 && ni<n && nj>=0 && nj<m && grid[ni][nj]==1 && !vis[ni][nj])
+                int nrow=row+dir[k].first,ncol=col+dir[k].second;
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] && 
+                   grid[nrow][ncol]==1)
                 {
-                    vis[ni][nj]=1;
-                    grid[ni][nj]=2;
-                    q.push({{ni,nj},time+1});
+                    q.push({time+1,{nrow,ncol}});
+                    vis[nrow][ncol]=1;
+                    grid[nrow][ncol]=2;
                 }
             }
         }
-        for(int i=0;i<n;++i)
+
+        for(int i=0;i<m;++i)
         {
-            for(int j=0;j<m;++j)
+            for(int j=0;j<n;++j)
             {
                 if(grid[i][j]==1)
                     return -1;
