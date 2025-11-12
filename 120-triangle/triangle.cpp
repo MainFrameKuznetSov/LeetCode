80348a5d1ec43be1c9b221183dba3bf09496f626
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-11-12 12:20:45 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (40.85%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<int>prev(1,triangle[0][0]);
+        vector<int>pre(n,0);
+        pre[0]=triangle[0][0];
         for(int i=1;i<n;++i)
         {
-            vector<int>curr(i+1,INT_MAX);
-            for(int j=0;j<i+1;++j)
+            vector<int>cur(n,0);
+            for(int j=0;j<=i;++j)
             {
-                if(j!=i)
-                    curr[j]=triangle[i][j]+prev[j];
-                if(j!=0)
-                    curr[j]=min(curr[j],triangle[i][j]+prev[j-1]);
+                if(j==i)
+                    cur[j]=INT_MAX;
+                else
+                    cur[j]=pre[j];
+                if(j-1>=0)
+                    cur[j]=min(cur[j],pre[j-1]);
+                cur[j]+=triangle[i][j];
             }
-            prev=vector<int>(i+1);
-            prev=curr;
+            pre=cur;
+            cur.clear();
         }
         int ans=INT_MAX;
-        for(int i:prev)
-            ans=min(ans,i);
+        for(int i=0;i<n;++i)
+        {
+            //cout<<pre[i]<<" ";
+            ans=min(ans,pre[i]);
+        }
         return ans;
     }
 };
