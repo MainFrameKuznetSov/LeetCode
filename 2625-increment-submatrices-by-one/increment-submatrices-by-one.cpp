367bf04b0dab92c05753a0722824c18ab69a678e
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-11-14 21:28:06 +0530

    Time: 1084 ms (14.42%) | Memory: 91.3 MB (47.34%) - LeetSync

---
+++ 2625-increment-submatrices-by-one/increment-submatrices-by-one.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+        vector<vector<int>>ans(n,vector<int>(n,0));
+        for(auto it:queries) 
+        {
+            int r1=it[0],c1=it[1],r2=it[2],c2=it[3];
+            for(int i=r1;i<=r2;++i) 
+            {
+                for(int j=c1;j<=c2;++j)
+                    ++ans[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
