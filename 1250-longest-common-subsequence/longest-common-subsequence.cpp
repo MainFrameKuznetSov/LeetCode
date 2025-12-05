792e623c3fa7b2a08b6ce49c64bf9a8324cbec5b
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-12-05 11:27:40 +0530

    Time: 25 ms (67.30%) | Memory: 26.5 MB (80.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -17,7 +17,19 @@ public:
 
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size(),m=text2.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        return f(n-1,m-1,text1,text2,dp);
+        vector<int>pre(m+1,0);
+        for(int i=1;i<=n;++i)
+        {
+            vector<int>cur(m+1,0);
+            for(int j=1;j<=m;++j)
+            {
+                if(text1[i-1]==text2[j-1])
+                    cur[j]=1+pre[j-1];
+                else
+                cur[j]=max(pre[j],cur[j-1]);
+            }
+            swap(pre,cur);
+        }
+        return pre[m];
     }
 };
