faf22d6437fab60be6e1016fc9fd847063fddf05
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-12-08 11:18:32 +0530

    Time: 6 ms (68.58%) | Memory: 7.8 MB (55.75%) - LeetSync

---
+++ 2037-count-square-sum-triples/count-square-sum-triples.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans=0;
+        for(int i=1;i<=n;++i)
+        {
+            for(int j=1;j<=n;++j)
+            {
+                int k=int(sqrt(i*i+j*j+1));
+                if(k<=n && k*k==i*i+j*j)
+                    ++ans;
+            }
+        }
+        return ans;
+    }
+};
