9e491093a2177f7950e5854b27260defbd769ec5
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2025-12-25 13:56:36 +0530

    Time: 201 ms (21.28%) | Memory: 110 MB (31.91%) - LeetSync

---
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.rbegin(),happiness.rend());
+        long long ans=0LL;
+        for(int i=0;i<k;++i)
+            ans+=(long long)max(0,happiness[i]-i);
+        return ans;
+    }
+};
