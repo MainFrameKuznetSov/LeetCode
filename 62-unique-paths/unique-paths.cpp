6026a93b3723111ecd820e8a4f2df8fa900cecd5
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-01-08 10:53:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (80.25%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int>prev(n,0);
-        prev[0]=1;
-        for(int i=0;i<m;++i)
+        vector<int>pre(n,1);
+        for(int i=1;i<n;++i)
+            pre[i]=1;
+        for(int i=1;i<m;++i)
         {
+            // dp[i][0]=1;
+            vector<int>cur(n,1);
             for(int j=0;j<n;++j)
             {
-                int up=0,left=0;
-                if(i==0 && j==0)
-                    continue;
-                if(i>0)
-                    up=prev[j];
-                if(j>0)
-                    left=prev[j-1];
-                prev[j]=up+left;                    
+                int up=pre[j],left=0;
+                if(j>=1)
+                    left=cur[j-1];
+                cur[j]=up+left;
             }
-            //prev=curr;
+            swap(pre,cur);
         }
-        return prev[n-1];
+        return pre[n-1];
     }
 };
