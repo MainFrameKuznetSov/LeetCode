80c17ab8a376060fbb5b9c075c1e6352145ca66b
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-01-08 11:02:29 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (76.60%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>&grid) {
-        int m=grid.size(),n=grid[0].size();
-        /*if(m==1 && n==1)
-        {   
-            if(grid[0][0]==1)
-                return 0;
-            else
-                return 1;
-        }*/
-        vector<int>prev(n,0);
-        if(grid[0][0]==1)
-            prev[0]=0;
-        else
-            prev[0]=1;
+        int n=grid.size(),m=grid[0].size();
+        vector<int>pre(m,0);
         for(int i=0;i<m;++i)
         {
-            for(int j=0;j<n;++j)
-            {
-                int up=0,left=0;
-                if(i==0 && j==0)
-                    continue;
-                if(i>0 && grid[i][j]!=1)
-                    up=prev[j];
-                if(j>0 && grid[i][j]!=1)
-                    left=prev[j-1];
-                prev[j]=up+left;
-            }
-            //prev=curr;
+            if(grid[0][i]==1)
+                break;
+            pre[i]=1;
         }
-        return prev[n-1];
+        for(int i=1;i<n;++i)
+        {
+            vector<int>cur(m,0);
+            cur[0]=pre[0];
+            for(int j=0;j<m;++j)
+            {
+                int up=pre[j],left=0;
+                if(j>=1)
+                    left=cur[j-1];
+                cur[j]=grid[i][j]==1?0:(up+left);
+            }
+            swap(pre,cur);
+        }
+        return pre[m-1];
     }
 };
