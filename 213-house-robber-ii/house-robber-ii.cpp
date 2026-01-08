30b422bbb1170c74f1282cf9abcb5b8109c7fabf
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-01-08 10:26:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (37.06%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
 
-    int f(int ind,vector<int>v,vector<int>&dp)
-    {   
-        if(ind>=v.size())
+    int f(int ind,vector<int>&vec,vector<int>&dp)
+    {
+        if(ind<0)
             return 0;
         if(dp[ind]!=-1)
             return dp[ind];
-        int take=v[ind]+f(ind+2,v,dp);
-        int notTake=f(ind+1,v,dp);
-        return dp[ind]=max(take,notTake);
+        int notTake=f(ind-1,vec,dp),Take=0;
+        Take=f(ind-2,vec,dp)+vec[ind];
+        return dp[ind]=max(notTake,Take);
     }
 
     int rob(vector<int>& nums) {
+        vector<int>v1,v2;
         int n=nums.size();
         if(n==1)
             return nums[0];
-        vector<int>v1,v2;
         for(int i=0;i<n;++i)
         {
             if(i!=0)
-                v2.push_back(nums[i]);
-            if(i!=n-1)
                 v1.push_back(nums[i]);
+            if(i!=n-1)
+                v2.push_back(nums[i]);
         }
-        vector<int>dp1(v1.size(),-1),dp2(v2.size(),-1);
-        f(0,v1,dp1);
-        f(0,v2,dp2);
-        return max(dp1[0],dp2[0]);
+        vector<int>dp1(n,-1),dp2(n,-1);
+        return max(f(v1.size()-1,v1,dp1),f(v2.size()-1,v2,dp2));
     }
 };
