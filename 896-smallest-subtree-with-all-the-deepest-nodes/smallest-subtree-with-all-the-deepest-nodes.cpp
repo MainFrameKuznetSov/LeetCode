a5387605232f45f45f58dac1db93a8ff1b26662d
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-01-09 21:34:23 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (27.77%) - LeetSync

---
+++ 896-smallest-subtree-with-all-the-deepest-nodes/smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    int maxD;
+    unordered_map<int,int>nodeMap;
+
+    void mapFiller(TreeNode* root,int depth)
+    {
+        if(!root)
+            return ;
+        nodeMap[root->val]=depth;
+        maxD=max(maxD,depth);
+        mapFiller(root->left,depth+1);
+        mapFiller(root->right,depth+1);
+    }
+
+    TreeNode* findLCA(TreeNode* root)
+    {
+        if(!root)
+            return NULL;
+        if(nodeMap[root->val]==maxD)
+            return root;
+        
+        TreeNode* leftNode=findLCA(root->left);
+        TreeNode* rightNode=findLCA(root->right);
+
+        if(leftNode && rightNode)
+            return root;
+        if(!leftNode)
+            return rightNode;
+        return leftNode;
+
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        nodeMap[root->val]=0;
+        maxD=0;
+        mapFiller(root,0);
+        return findLCA(root);
+    }
+};
