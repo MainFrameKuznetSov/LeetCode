c399c32eb104eecca20868aedff80f07d746cf9f
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-01-29 22:04:19 +0530

    Time: 38 ms (55.20%) | Memory: 100.1 MB (70.90%) - LeetSync

--- 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/minimum-cost-to-convert-string-i.cpp
@@ -1,31 +1,33 @@
+#define ll long long
+
 class Solution {
 public:
-    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+    ll minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         int n=original.size();
-        vector<vector<long long>>graph(26,vector<long long>(26,INT_MAX));
+        vector<vector<ll>>graph(26,vector<ll>(26,INT_MAX));
         for(int i=0;i<n;++i) 
-            graph[original[i]-'a'][changed[i]-'a']=min(graph[original[i] - 'a'][changed[i]-'a'],(long long)cost[i]);
+            graph[original[i]-'a'][changed[i]-'a']=min(graph[original[i]-'a'][changed[i]-'a'],(ll)cost[i]);
         for(int k=0;k<26;++k) 
         {
             for(int i=0;i<26;++i) 
             {
                 for(int j=0;j<26;++j) 
                 {
-                    if ((graph[k][j]!=INT_MAX && graph[i][k]!=INT_MAX) && graph[i][j]>(long long)(graph[i][k]+graph[k][j]))
-                       graph[i][j]=(long long)(graph[i][k]+graph[k][j]);
+                    if ((graph[k][j]!=INT_MAX && graph[i][k]!=INT_MAX) 
+                      && graph[i][j]>(ll)(graph[i][k]+graph[k][j]))
+                       graph[i][j]=(ll)(graph[i][k]+graph[k][j]);
                 }
             }
         }
-        long long ans=0;
+        ll ans=0;
         for(int i=0;i<source.size();++i) 
         {
-            int a=source[i]-'a';
-            int b=target[i]-'a';
+            int a=source[i]-'a',b=target[i]-'a';
             if(a!=b) 
             {
                 if(graph[a][b]==INT_MAX) 
                     return -1;
-                ans=(long long)(ans+graph[a][b]);
+                ans=(ll)(ans+graph[a][b]);
             }
         }
         return ans;
