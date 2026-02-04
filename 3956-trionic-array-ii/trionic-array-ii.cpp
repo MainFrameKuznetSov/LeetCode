f7444eb419eeb632e807372ca57c40451cc5519d
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-02-04 22:00:29 +0530

    Time: 4 ms (89.09%) | Memory: 132.6 MB (91.82%) - LeetSync

---
+++ 3956-trionic-array-ii/trionic-array-ii.cpp
@@ -0,0 +1,46 @@
+#define ll long long
+
+class Solution {
+public:
+    long long maxSumTrionic(vector<int>& nums) {
+        int n=nums.size();
+        ll res=-2e18;
+        for(int i=1;i+2<n;) 
+        {
+            int a=i,b=i;
+            ll net=nums[a];
+            while(b+1<n && nums[b+1]<nums[b])
+                net+=nums[++b];
+            if(b==a) 
+            { 
+                ++i; 
+                continue; 
+            }
+            int c=b;
+            ll left=0,right=0,lx=-2e18,rx=-2e18;
+            while(a-1>= 0 && nums[a-1]<nums[a]) 
+            {
+                left+=nums[--a];
+                lx=max(lx, left);
+            }
+            if(a==i) 
+            {   
+                ++i; 
+                continue; 
+            }
+            while(b+1<n && nums[b+1]>nums[b]) 
+            {
+                right+=nums[++b];
+                rx=max(rx,right);
+            }
+            if(b==c) 
+            { 
+                ++i; 
+                continue; 
+            }
+            res=max(res,lx+rx+net);
+            i=b;
+        }
+        return res;
+    }
+};
