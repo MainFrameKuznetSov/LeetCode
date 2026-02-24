503b9c178214c7fb5dcc406e54f8c41e195d2c16
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-02-24 14:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (34.02%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -12,51 +12,17 @@
 class Solution {
 public:
 
-    vector<string>collectedStrings;
-
-    bool isLeaf(TreeNode* root)
+    int dfs(TreeNode* root,int sum)
     {
-        return (!(root->left) && !(root->right));
+        if(!root)
+            return 0;
+        sum=sum*2+(root->val);
+        if(!(root->left) && !(root->right))
+            return sum;
+        return dfs(root->left,sum)+dfs(root->right,sum);
     }
 
-    void recur(TreeNode* root,string binary)
-    {
-        bool f=0;
-        if(isLeaf(root))
-        {
-            binary+=to_string(root->val);
-            collectedStrings.push_back(binary);
-        }
-        if(root->left)
-        {
-            f=1;
-            binary+=to_string(root->val);
-            recur(root->left,binary);
-        }
-        if(f)
-            binary.pop_back();
-        if(root->right)
-        {
-            binary+=to_string(root->val);
-            recur(root->right,binary);
-        }
-    }
-
-    int sumRootToLeaf(TreeNode* root) 
-    {
-        recur(root,"");
-        int n=collectedStrings.size(),ans=0;
-        for(string str:collectedStrings)
-        {
-            int sum=0,ind=0;
-            for(int j=str.size()-1;j>=0;--j)
-            {
-                int digit=str[j]-'0';
-                sum+=(digit<<ind);
-                ++ind;
-            }
-            ans+=sum;
-        }
-        return ans;
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root,0);
     }
 };
