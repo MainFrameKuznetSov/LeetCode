796ce98d7633e92caf0ea639761f1636172d60c4
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2026-02-25 11:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (58.88%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        vector<pair<int,int>>pr;
-        for(int i:arr)
-            pr.push_back({i,__builtin_popcount(i)});
         sort(arr.begin(),arr.end(),[&](int a,int b)
         {
             int bitA=__builtin_popcount(a),bitB=__builtin_popcount(b);
